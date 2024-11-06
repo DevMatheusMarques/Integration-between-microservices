8d8b2187dcdc7e000da36056efd52ae3a97eb95e
DevMatheusMarques <estudosfatecmhcm@gmail.com>
2024-11-06 15:05:12 -0300

    feat(ms-usuario): Finalized exception classes.

---
+++ ms-usuario/src/main/java/com/compass/ms_usuario/exceptions/UserAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.compass.ms_usuario.exceptions;
+
+public class UserAlreadyExistsException extends RuntimeException {
+
+    public UserAlreadyExistsException() {
+        super("User already exists.");
+    }
+}
